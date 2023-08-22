a9328b1057b79bcc0d15eefcedabf7e43081e475
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-08-22 18:45:42 +0200

    milestone 4

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <h1>Hello, World!</h1>
+</template>
+
+
+<script>
+  // import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
+
+
+
+  export default{
+    name: 'App',
+    mounted(){
+      axios.get('http://localhost:8000/api/v1/projects')
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => console.error(err));
+    }
+  }
+</script>
