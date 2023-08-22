a04d6babdfad99f2d544606ba11bbad5cf9a10f7
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-08-23 00:20:28 +0200

    Creazione della view + singola card

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 
 <template>
-  <h1>Hello, World!</h1>
+  <h1 style="color: blue;">Projects</h1>
+  <div class="row justify-content-between">
+    <project-card 
+    v-for="project in projects"
+    :key="project.id" 
+    :project="project"
+    class="col-3 mx-2 my-3"
+    />
+  </div>
 </template>
 
 
 <script>
   // import HelloWorld from './components/HelloWorld.vue'
-  import axios from 'axios';
-
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue'  
 
 
   export default{
     name: 'App',
+    components: {
+      ProjectCard
+    },
+    data(){
+      return{
+        projects: []
+      }
+    },
     mounted(){
       axios.get('http://localhost:8000/api/v1/projects')
         .then(res => {
-          console.log(res.data);
+          this.projects = res.data.projects;
+          console.log(JSON.stringify(this.projects, null, 2));
         })
         .catch(err => console.error(err));
     }
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <h1>{{project.name }}</h1>
+        <p>{{ project.difficulty }}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ProjectCard',
+    props: {
+        project:{
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+  
+</style>
